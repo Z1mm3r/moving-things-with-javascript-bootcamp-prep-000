d96aa489b292ed29936c2e33b72afd897b2d6efe
Casey Zimmer <czimmer@ramapo.edu>
2018-08-15 19:54:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
  
-  if (left < 360) {
+  if (right < 360) {
     dodger.style.left = `${left + 1}px`
   }
 }
